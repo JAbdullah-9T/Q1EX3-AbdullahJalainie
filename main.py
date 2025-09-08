6c887c05b5c7d6f2511356937cedf9a3082915a4
JAbdullah-9T <abdullah.jr@obmontessori.edu.ph>
2025-09-08 09:06:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# PYTHON
+from pyscript import display
+
+
+first_name = 'Jalainie Radia' #string
+last_name = 'Abdullah' #string
+age = '15' #integer
+weight = '56.09' #float
+is_new_student = 'false' #boolean
+sections = ['Topaz', 'Emerald', 'Ruby', 'Sapphire'] #list
+
+display(f'My name is {first_name} {last_name}', target='div1')
